054d845a28ad2770b7bbbc750e221b6144083456
tristangclvs <goncalvestristan.pro@pm.me>
2023-04-04 00:03:43 +0200

    css update of Login Page

--- screens/LoginPage.js
+++ screens/LoginPage.js
@@ -23,81 +23,76 @@ const LoginPage = ({ navigation }) => {
     // regular expression for email format checking
     const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
     setIsEmailValid(emailRegex.test(text));
+    console.log(isEmailValid);
   };
-  // const InputDisplay = ({ type }) => {
-  //   return type == "email" ? (
-  //     <View style={styles.inputContainer}>
-  //       <Text>Email</Text>
-  //       <View style={styles.inputSection}>
-  //         {/* <Image
-  //         style={{ width: 20, height: 20 }}
-  //         source={require("../resources/images/email.png")}
-  //       /> */}
-  //         <TextInput
-  //           style={styles.input}
-  //           onChangeText={handleEmailChange}
-  //           value={""}
-  //           placeholder="Enter your email address"
-  //           keyboardType="numeric"
-  //         />
-  //       </View>
-  //     </View>
-  //   ) : (
-  //     <View style={styles.inputContainer}>
-  //       <Text>Email</Text>
-  //       <View style={styles.inputSection}>
-  //         {/* <Image
-  //         style={{ width: 20, height: 20 }}
-  //         source={require("../resources/images/email.png")}
-  //       /> */}
-  //         <TextInput
-  //           style={styles.input}
-  //           onChangeText={handleEmailChange}
-  //           value={""}
-  //           placeholder="Enter your email address"
-  //           keyboardType="numeric"
-  //         />
-  //       </View>
-  //     </View>
-  //   );
-  // };
+  const handlePasswordChange = (text) => {
+    setPassword(text);
+  };
+
+
+  const InputDisplay = ({ type }) => {
+    return type == "email" ? (
+        <View style={styles.inputContainer}>
+          <Text style={{color:'#2B5453', marginLeft:3}}>Email</Text>
+          <View style={styles.inputSection}>
+            {/* <Image
+          style={{ width: 20, height: 20 }}
+          source={require("../resources/images/email.png")}
+        /> */}
+            <TextInput
+                style={styles.input}
+                onSubmitEditing={(event) => {handleEmailChange(event.nativeEvent.text);}}
+                placeholder="Enter your email address"
+            />
+          </View>
+        </View>
+    ) : (
+        <View style={styles.inputContainer}>
+          <Text style={{color:'#2B5453', marginLeft:3}}>Password</Text>
+          <View style={styles.inputSection}>
+            {/* <Image
+          style={{ width: 20, height: 20 }}
+          source={require("../resources/images/email.png")}
+        /> */}
+            <TextInput
+                style={styles.input}
+                onSubmitEditing={(event) => {handlePasswordChange(event.nativeEvent.text);}}
+                secureTextEntry={true}
+                placeholder="Password"
+            />
+          </View>
+        </View>
+    );
+  };
+
   return (
-    <View>
-      <Image
-        style={{ width: 100, height: 100, marginTop: 50 }}
-        source={require("../resources/images/logo_green.png")}
-      />
-      <Text>Login to WorkAdventure</Text>
-      <View style={styles.inputContainer}>
-        <Text>Email</Text>
-        <View style={styles.inputSection}>
-          {/* <Image
-            style={{ width: 20, height: 20 }}
-            source={require("../resources/images/email.png")}
-          /> */}
-          <TextInput
-            style={styles.input}
-            onChangeText={handleEmailChange}
-            value={""}
-            placeholder="Enter your email address"
-            keyboardType="numeric"
-          />
+      <View style={styles.container}>
+        <Image
+            style={{ width: 100, height: 100, marginTop: 50,objectFit: "contain"}}
+            source={require("../resources/images/logo_green.png")}
+        />
+        <Text style={styles.titleLogIn}>Login to WorkAdventure</Text>
+        <InputDisplay type={"email"}/>
+        <InputDisplay type={"password"}/>
+
+        <TouchableOpacity style={styles.loginButton} >
+          <Text style={styles.loginButtonText}>Log In</Text>
+        </TouchableOpacity>
+        <View style={styles.separatorContainer}>
+          <View style={styles.horizontalBar}></View>
+          <Text style={styles.horizontalBarText}>Or login with</Text>
+          <View style={styles.horizontalBar}></View>
         </View>
-      </View>
-      <View style={styles.inputContainer}>
-        <Text>Password</Text>
-        <View style={styles.inputSection}>
-          <TextInput
-            style={styles.input}
-            onChangeText={(text) => setPassword(text)}
-            value={""}
-            secureTextEntry={true}
-            placeholder="Password"
-            keyboardType="numeric"
-          />
+
+        <View style={styles.signUpContainer}>
+          <Text>Don't have an account yet ?</Text>
+          <TouchableOpacity  onPress={() => navigation.navigate("SignUp")}>
+            <Text style={styles.signUpLink}>Sign Up</Text>
+          </TouchableOpacity>
         </View>
+
       </View>
-    </View>
+
   );
 };
 
@@ -106,17 +101,66 @@ export default LoginPage;
 // Use StylesSheet flatten
 const styles = StyleSheet.create({
   container: {
+    marginTop: 50,
     height: "100%",
     alignItems: "center",
   },
+  titleLogIn: {
+    fontSize: 30,
+    marginBottom: 20,
+  },
   inputContainer: {
     width: "80%",
+    marginBottom: 20,
+    marginTop: 20,
   },
   input: {
     padding: 10,
     height: 40,
     borderRadius: 10,
-    borderWidth: 1,
+    borderWidth: 2,
     borderColor: "#234F52",
+  },
+  loginButton: {
+    backgroundColor: "#234F52",
+    padding: 10,
+    marginTop: 30,
+    borderRadius: 10,
+    width: "80%",
+    height: 60,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  loginButtonText: {
+    color: "white",
+    fontSize: 25,
+  },
+  separatorContainer: {
+    position: "relative",
+    flexDirection: "row",
+  },
+  horizontalBar: {
+    width: "25%",
+    height: 1,
+    backgroundColor: "#234F52",
+    marginTop: 40,
+  },
+
+  horizontalBarText: {
+    top: 30,
+    justifyContent: "center",
+    color: "#234F52",
+    marginHorizontal: 10,
+  },
+  signUpContainer: {
+    position: "absolute",
+    bottom: 100,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  signUpLink: {
+    color:'#00B4AF',
+    fontSize: 20,
+    fontWeight: "500",
   },
 });
