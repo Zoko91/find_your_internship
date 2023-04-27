5d449e31a407758a74ba1b30cbd0629b45138479
tristangclvs <goncalvestristan.pro@pm.me>
2023-04-27 22:19:40 +0200

    ProfilePage shortened

---
+++ components/ProfilePicture.js
@@ -0,0 +1,21 @@
+import {Image} from "react-native";
+import styles from "../theme/style";
+import React from "react";
+
+const ProfilePicture = ({profilePic}) => {
+    return profilePic === null ? (
+        <Image
+            style={styles.userProfilePicture}
+            source={require("../resources/images/PDP.png")}
+        />
+    ) : (
+        <Image
+            style={styles.userProfilePicture}
+            source={{
+                uri: profilePic,
+            }}
+        />
+    );
+};
+
+export default ProfilePicture;
--- screens/ProfilePage.js
+++ screens/ProfilePage.js
@@ -1,219 +1,67 @@
-import React, { useState, useEffect } from "react";
-import { useFocusEffect } from "@react-navigation/native";
-import { Text, View, ScrollView, Image, TouchableOpacity } from "react-native";
+import React, {useState, useEffect} from "react";
+import {useFocusEffect} from "@react-navigation/native";
+import {Text, View, ScrollView, Image, TouchableOpacity} from "react-native";
 import UsersService from "../api/UsersService";
 import styles from "../theme/style.js";
 import LoadingPage from "./LoadingPage";
+import ProfilePicture from "../components/ProfilePicture";
+import UserHeader from "../components/UserHeader";
+import Support from "../components/Support";
+import AccountSettings from "../components/AccountSettings";
 
-const ProfilePage = ({ navigation, route }) => {
-  const [user, setUser] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  useFocusEffect(
-    // --------------------- Explication  ---------------------
-    // Ce hook permet de recharger le composant à chaque fois que l'on arrive sur la page
-    // Très important pour permettre de modifier l'image de profil
-    React.useCallback(() => {
-      getUser();
-      return () => {
-        // This code will be executed when the screen loses focus
-        // You can use it to clean up any resources that were allocated in the callback
-      };
-    }, [])
-  );
-
-  const getUser = async () => {
-    try {
-      const response = await UsersService.findUserById(
-        route.params.usertest.id
-      );
-      setUser(response);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const ProfilePicture = ({ profilePic }) => {
-    return profilePic === null ? (
-      <Image
-        style={styles.userProfilePicture}
-        source={require("../resources/images/PDP.png")}
-      />
-    ) : (
-      <Image
-        style={styles.userProfilePicture}
-        source={{
-          uri: profilePic,
-        }}
-      />
+const ProfilePage = ({navigation, route}) => {
+    const [user, setUser] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    useFocusEffect(
+        // --------------------- Explication  ---------------------
+        // Ce hook permet de recharger le composant à chaque fois que l'on arrive sur la page
+        // Très important pour permettre de modifier l'image de profil
+        React.useCallback(() => {
+            getUser();
+            return () => {
+                // This code will be executed when the screen loses focus
+                // You can use it to clean up any resources that were allocated in the callback
+            };
+        }, [])
     );
-  };
 
-  const navigate = (name, func) => {
-    navigation.navigate(name);
-  };
+    const getUser = async () => {
+        try {
+            const response = await UsersService.findUserById(
+                route.params.usertest.id
+            );
+            setUser(response);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-  useEffect(() => {
-    getUser();
-  }, []);
-  useEffect(() => {
-    getUser();
-  }, [navigation]);
 
-  useEffect(() => {
-    setIsLoading(false);
-  }, [user]);
+    const navigate = (name) => {
+        navigation.navigate(name);
+    };
 
-  const UserHeader = () => {
-    return (
-      <View>
-        <TouchableOpacity
-          style={styles.containerHeaderUser}
-          onPress={() => {
-            navigate("Details");
-          }}
-        >
-          <View style={styles.containerHeaderUser1}>
-            <ProfilePicture profilePic={user.avatar} />
-          </View>
-          <View style={styles.containerHeaderUser2}>
-            <Text style={styles.nameUser}>{user.username}</Text>
-            <Text style={styles.showProfile}>Show profile</Text>
-          </View>
-          <View style={styles.containerHeaderUser3}>
-            <Image
-              style={styles.arrowUser}
-              source={require("../resources/images/Chevron.png")}
-            />
-          </View>
-        </TouchableOpacity>
-        <View style={styles.fineLine}></View>
-      </View>
-    );
-  };
+    useEffect(() => {
+        getUser();
+    }, []);
+    useEffect(() => {
+        getUser();
+    }, [navigation]);
+    useEffect(() => {
+        setIsLoading(false);
+    }, [user]);
+    
+    const UserPage = () => {
+        return (
+            <ScrollView style={{flex: 1}}>
+                <UserHeader navigate={navigate} user={user}/>
+                <AccountSettings navigate={navigate}/>
+                <Support navigate={navigate}/>
+            </ScrollView>
+        );
+    };
 
-  const AccountSettings = () => {
-    return (
-      <View>
-        <View style={styles.containerAccount}>
-          <Text style={styles.headernameUser}>Account Settings</Text>
-          <TouchableOpacity
-            style={styles.containerAccountInside}
-            onPress={() => {
-              navigate("Informations");
-            }}
-          >
-            <Image
-              style={styles.iconUser}
-              source={require("../resources/images/PersonIcon.png")}
-            />
-            <Text style={{ marginTop: 6 }}>Personal Information</Text>
-            <View style={styles.containerArrowSetting}>
-              <Image
-                style={styles.arrowUserSetting}
-                source={require("../resources/images/Chevron.png")}
-              />
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.containerAccountInside}
-            onPress={() => {
-              navigate("Internships");
-            }}
-          >
-            <Image
-              style={styles.iconUser}
-              source={require("../resources/images/InternPhoto.png")}
-            />
-            <Text style={{ marginTop: 6 }}>My Internships</Text>
-            <View style={styles.containerArrowSetting}>
-              <Image
-                style={styles.arrowUserSetting}
-                source={require("../resources/images/Chevron.png")}
-              />
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.containerAccountInside}
-            onPress={() => {
-              navigate("Bookmarks");
-            }}
-          >
-            <Image
-              style={styles.iconUser}
-              source={require("../resources/images/Bookmark.png")}
-            />
-            <Text style={{ marginTop: 6 }}>My bookmarks</Text>
-            <View style={styles.containerArrowSetting}>
-              <Image
-                style={styles.arrowUserSetting}
-                source={require("../resources/images/Chevron.png")}
-              />
-            </View>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.fineLine}></View>
-      </View>
-    );
-  };
-  const Support = () => {
-    return (
-      <View>
-        <View style={styles.containerAccount}>
-          <Text style={styles.headernameUser}>Support</Text>
-          <TouchableOpacity
-            style={styles.containerAccountInside}
-            onPress={() => {
-              navigate("Privacy");
-            }}
-          >
-            <Image
-              style={styles.iconUser}
-              source={require("../resources/images/files.png")}
-            />
-            <Text style={{ marginTop: 6 }}>Pivacy policy</Text>
-            <View style={styles.containerArrowSetting}>
-              <Image
-                style={styles.arrowUserSetting}
-                source={require("../resources/images/Chevron.png")}
-              />
-            </View>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={styles.containerAccountInside}
-            onPress={() => {
-              navigate("Help");
-            }}
-          >
-            <Image
-              style={styles.iconUser}
-              source={require("../resources/images/help.png")}
-            />
-            <Text style={{ marginTop: 6 }}>Get help</Text>
-            <View style={styles.containerArrowSetting}>
-              <Image
-                style={styles.arrowUserSetting}
-                source={require("../resources/images/Chevron.png")}
-              />
-            </View>
-          </TouchableOpacity>
-        </View>
-        {/* <View style={styles.fineLine}></View> */}
-      </View>
-    );
-  };
-
-  const UserPage = () => {
-    return (
-      <ScrollView style={{ flex: 1 }}>
-        <UserHeader />
-        <AccountSettings />
-        <Support />
-      </ScrollView>
-    );
-  };
-
-  return isLoading ? <LoadingPage /> : <UserPage />;
+    return isLoading ? <LoadingPage/> : <UserPage/>;
 };
 
 export default ProfilePage;
